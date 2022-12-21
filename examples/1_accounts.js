5f3a1f27da2524f35dd401c03ecfb293a6fde6f9
Anton Laza <anton.laza.0116@gmail.com>
2022-12-21 08:33:05 +0100

    checkBalance() implemented

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,5 +1,12 @@
 const { ethers } = require("ethers");
-
 const rpcURL = "https://cloudflare-eth.com/"
-
 const provider = new ethers.providers.JsonRpcProvider(rpcURL);
+
+const address = "0x101D7eD3eF83b58B82892C773793b49D0a456a00"
+
+const checkBalance = async () => {
+    const balance = await provider.getBalance(address)
+    console.log(balance)
+}
+
+checkBalance()
