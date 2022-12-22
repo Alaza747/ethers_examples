d07f655d7d05371714e4a0d60019167ed04f6099
Anton Laza <anton.laza.0116@gmail.com>
2022-12-22 08:39:30 +0100

    added getBlockNumber()

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -16,9 +16,12 @@ const main = async () => {
     const name = await contract.name();
     const symbol = await contract.symbol();
     const totalSupply = await contract.totalSupply();
-    const balanceOf = await contract.balanceOf(address);
-    console.log(name, symbol, totalSupply, ethers.utils.formatEther(balanceOf));
+    const balanceOf = await contract.balanceOf("0x9a315bdf513367c0377fb36545857d12e85813ef");
+    console.log(name, symbol, totalSupply,toString())
+    console.log(ethers.utils.formatEther(balanceOf));
 
+    const blockNumber = await provider._getFastBlockNumber() 
+    console.log(blockNumber)
 
 }
 
